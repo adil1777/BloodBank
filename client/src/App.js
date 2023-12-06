86ebaff206c36e54e864e1a6346c872ccb939c6f
Mohd Adil <adilazmi760@gmail.com>
2023-12-06 22:22:29 +0530

    add browseRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -19,116 +20,118 @@ import AdminHome from "./pages/Admin/AdminHome";
 
 function App() {
   return (
-    <>
-      <ToastContainer />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          }
-        />
-         <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminHome />
-            </ProtectedRoute>
-          }
-        />
-         <Route
-          path="/analytics"
-          element={
-            <ProtectedRoute>
-              <Analytics />
-            </ProtectedRoute>
-          }
-        />
-         <Route
-          path="/donar"
-          element={
-            <ProtectedRoute>
-              <Donar />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/donar-list"
-          element={
-            <ProtectedRoute>
-              <DonarList/>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/hospital-list"
-          element={
-            <ProtectedRoute>
-              <HospitalList/>
-            </ProtectedRoute>
-          }
-        />
-         <Route
-          path="/org-list"
-          element={
-            <ProtectedRoute>
-              <OrgList/>
-            </ProtectedRoute>
-          }
-        />
+    <Router>
+      <>
+        <ToastContainer />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminHome />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/analytics"
+            element={
+              <ProtectedRoute>
+                <Analytics />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/donar"
+            element={
+              <ProtectedRoute>
+                <Donar />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/donar-list"
+            element={
+              <ProtectedRoute>
+                <DonarList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/hospital-list"
+            element={
+              <ProtectedRoute>
+                <HospitalList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/org-list"
+            element={
+              <ProtectedRoute>
+                <OrgList />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/hospital"
-          element={
-            <ProtectedRoute>
-              <Hospitals />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/organisation"
-          element={
-            <ProtectedRoute>
-              <OrganisationPage/>
-            </ProtectedRoute>
-          }
-        />
-         <Route
-          path="/consumer"
-          element={
-            <ProtectedRoute>
-              <Consumer />
-            </ProtectedRoute>
-          }
-        />
-         <Route
-          path="/donation"
-          element={
-            <ProtectedRoute>
-              <Donation/>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-      </Routes>
-    </>
+          <Route
+            path="/hospital"
+            element={
+              <ProtectedRoute>
+                <Hospitals />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/organisation"
+            element={
+              <ProtectedRoute>
+                <OrganisationPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/consumer"
+            element={
+              <ProtectedRoute>
+                <Consumer />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/donation"
+            element={
+              <ProtectedRoute>
+                <Donation />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
